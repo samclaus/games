b4968982009eb72d4d0319f5462405c3d81b6cca
Sam Claus <samclaus@protonmail.com>
2024-02-13 20:22:40 -0500

    Start work on example usage

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/samclaus/games"
+	"github.com/samclaus/games/bravewength"
+)
+
 func main() {
-	// TODO
+	http.ListenAndServe(":8080", games.NewServer(
+		websocket.Upgrader{},
+		bravewength.Game,
+	))
 }
